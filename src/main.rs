1ed5b3d2be06ef057bab38839be5eda85e8b84e5
krabiworld <krabiworld@proton.me>
2025-04-17 20:38:05 +0300

    Fix bind address

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(webhook)
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
